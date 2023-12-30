b0198f500c791034f0f0dda44ce7d8c6ce283ffd
ahmedyousef0412 <37086138+ahmedyousef0412@users.noreply.github.com>
2023-12-30 22:03:24 +0200

    Add project files.

---
+++ Secure-The-Code/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Secure_The_Code.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
